a79829eee8e80157051c7b8b035c7c9f26a280d8
end2you <35073284+end2you@users.noreply.github.com>
2018-01-03 21:42:25 +0000

    Update data_provider.py

--- src/data_provider/data_provider.py
+++ src/data_provider/data_provider.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
@@ -15,13 +16,15 @@ class DataProvider(metaclass=ABCMeta):
                  batch_size:int = 32,
                  seq_length:int = None):
         
-        self.frame_shape = frame_shape
-        self.label_shape = label_shape
+        tf_records = [str(x) for x in tfrecords_folder.glob('*.tfrecords')][0]
+        
+        self.frame_shape = self.get_shape(tf_records, 'frame')
+        self.label_shape = self.get_shape(tf_records, 'label')
         self.seq_length = seq_length
         self.batch_size = batch_size
         self.is_training = is_training
         
-        self.root_path = Path(tfrecords_folder) / split_name
+        self.root_path = Path(tfrecords_folder)
         paths = [str(x) for x in self.root_path.glob('*.tfrecords')]
         
         filename_queue = tf.train.string_input_producer(paths, shuffle=is_training)
@@ -34,6 +37,20 @@ class DataProvider(metaclass=ABCMeta):
     def parse_and_decode_example(self):
         pass
     
+    def get_shape(self, input_files, tensor_name):
+        record_iterator = tf.python_io.tf_record_iterator(path=str(input_files))
+        
+        for string_record in record_iterator:
+            example = tf.train.Example()
+            example.ParseFromString(string_record)
+            tt = (example.features.feature[tensor_name]
+                                         .bytes_list
+                                         .value[0])
+            input_shape = np.fromstring(tt, dtype=np.float32)
+            break
+        
+        return input_shape.shape[0]
+
     def augment_data(self):
         raise NotImplementedError()
     
