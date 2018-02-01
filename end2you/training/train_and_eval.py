ade16bbf21985d4a84fcef9417cdfc3357f0980e
tzirakis <ptzirakis88@yahoo.com>
2018-02-01 15:59:16 +0000

    Update train_and_eval.py

--- end2you/training/train_and_eval.py
+++ end2you/training/train_and_eval.py
@@ -23,6 +23,8 @@ class TrainEval(Train):
         
         super().__init__(*args, **kwargs)
         
+        self.train_dir = Path(self.train_dir) / 'ckpt'
+        
         self.save_top_k = 5 #kwargs['save_top_k']
         self.save_dir = Path(self.train_dir) / 'top_saved_models'
         
@@ -37,6 +39,8 @@ class TrainEval(Train):
         else:
             self.best_perfs = {str(x):float('-inf') for x in np.arange(self.save_top_k)}
             os.system('mkdir -p {}'.format(self.save_dir))
+        
+        self.log_dir = self.train_dir / 'log'
         
     def _restore_variables(self, sess, saver):
         model_path = tf.train.latest_checkpoint(self.train_dir)
