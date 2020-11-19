28ecc1d7e15bbdb82e3cb9a2b0c166c889f66af4
Panagiotis Tzirakis <pt511@aracari.doc.ic.ac.uk>
2020-11-19 22:44:34 +0000

    change __init__ files

--- end2you/__init__.py
+++ end2you/__init__.py
@@ -1,8 +1,9 @@
-from .data_generator import *
-from .data_provider import *
-from .models import *
-from .training import *
-from .evaluation import *
+import end2you.data_generator
+import end2you.data_provider
+import end2you.models
+import end2you.training
+import end2you.evaluation
+
 from .generation_process import GenerationProcess
 from .training_process import TrainingProcess
 from .evaluation_process import EvaluationProcess
