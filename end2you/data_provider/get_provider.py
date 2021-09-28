1f01ddd2a0ed9895a29dd778489959e1bffb808c
RosaStamp <rosa_stampolidou@hotmail.gr>
2021-09-28 23:12:21 +0300

    updating base methods

--- end2you/data_provider/get_provider.py
+++ end2you/data_provider/get_provider.py
@@ -1,15 +1,14 @@
-import torch
-
-from torch.utils.data import DataLoader
-from torch.nn.utils.rnn import pad_sequence
+from . import raw
+from . import hdf5
 from functools import partial
-from .audio_provider import AudioProvider
-from .visual_provider import VisualProvider
-from .multifile_audiovisual_provider import MultiFile_AVProvider
-from .singlefile_audiovisual_provider import SingleFile_AVProvider
 
+def get_proper_provider(files_format):
+    return {
+        'raw': get_raw_provider,
+        'hdf5': get_hdf5_provider
+    }[files_format]
 
-def get_provider(modality):
+def get_hdf5_provider(modality):
     """ Factory method to get the appropriate provider.
         
     Args:
@@ -17,69 +16,29 @@ def get_provider(modality):
     """
     
     return {
-        'audio': AudioProvider,
-        'visual': VisualProvider,
-        'audiovisual': SingleFile_AVProvider
+        'audio': hdf5.AudioProvider,
+        'visual': hdf5.VisualProvider,
+        'audiovisual': hdf5.SingleFile_AVProvider
+    }[modality]
+
+def get_raw_provider(modality):
+    """ Factory method to get the appropriate provider.
+    
+    Args:
+      modality (str): Which modality provider to return.
+    """
+    file_provider = _get_file_provider(modality)
+    
+    return {
+        'audio': partial(raw.AudioProvider, file_provider=file_provider),
+        'visual': partial(raw.VisualProvider, file_provider=file_provider),
+#         'audiovisual': raw.SingleFile_AVProvider
     }[modality]
 
 
-def pad_collate(batch):
-    """ Pad batch tensors to have equal length.
-    
-    Args:
-        batch (list): Data to pad.
-    
-    Returns:
-        modality_tensors (torch.Tensor): Batched data tensors. 
-        labels (torch.Tensor): Batched label tensors.
-        num_seqs_per_sample (list): Number of sequences of each batch tensor.
-        data_file (str): File name.
-    """
-    
-    data, labels, data_file = zip(*batch)
-    
-    number_of_modalities = len(data[0]) if isinstance(data[0], list) else 1
-    if number_of_modalities == 1:
-        data = [[x] for x in data]
-
-    modality_tensors = []
-    for i in range(number_of_modalities):
-        modality_i = [torch.Tensor(x[i]) for x in data]
-        padded_modality = pad_sequence(modality_i, batch_first=True)
-        modality_tensors.append(padded_modality)
-    
-    num_seqs_per_sample = [len(x) for x in labels]
-    labels = [torch.Tensor(x) for x in labels]
-    labels = pad_sequence(labels, batch_first=True)
-    
-    if number_of_modalities == 1:
-        modality_tensors = modality_tensors[0]
-    
-    return modality_tensors, labels, num_seqs_per_sample, data_file
-
-
-def get_dataloader(params, **kwargs):
-    """ Gets the DataLoader object for each type in split_dirs keys.
-    
-    Args:
-      params (Params) : Parameters needed to load data.
-        `modality` (str): Modality to provide data from.
-        `dataset_path` (str): Path to `hdf5` data files.
-        `seq_length` (int): Number of consecuvite frames to load.
-        `batch_size` (int): Batch size.
-        `cuda` (int): Whether to use cuda
-        `num_workers` (int): Number of workers to use.
-        `is_training` (bool): Whether to provide data for training/evaluation.
-    
-    Returns:
-      dataloaders (dict): contains the DataLoader object for each type 
-                          in `split_dirs` keys.
-    """
-    
-    Provider = get_provider(params.modality)
-    return  DataLoader(Provider(params.dataset_path, seq_length=params.seq_length),
-                       batch_size=params.batch_size,
-                       shuffle=params.is_training,
-                       num_workers=params.num_workers,
-                       pin_memory=params.cuda,
-                       collate_fn=pad_collate)
+def _get_file_provider(modality):
+    return {
+        'audio': raw.AudioFileProvider,
+        'visual': raw.VisualFileProvider,
+#         'audiovisual': AVFileProvider
+    }[modality]
